b9dc152ccb757286de79ce09fb9a15ff419b074f
ANDREOGP33 <andregasparoni690@gmail.com>
2023-12-19 14:10:22 -0300

    Populando a tabela lanches com dados iniciais

---
+++ LanchesMac/Context/AppDbcontext.cs
@@ -0,0 +1,17 @@
+﻿using LanchesMac.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace LanchesMac.Context
+{
+    public class AppDbcontext : DbContext
+    {
+        public AppDbcontext(DbContextOptions<AppDbcontext> options) : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias {  get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+
+    }
+}
