4f0fe9f536c981d20a64a743e9b625caf5f3ab2b
ANDREOGP33 <andregasparoni690@gmail.com>
2023-12-19 17:00:40 -0300

    Implemetando o padrao Repository para lanches

---
+++ LanchesMac/Repositories/CategoriaRepository.cs
@@ -0,0 +1,18 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+
+namespace LanchesMac.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private readonly AppDbcontext _context;
+
+        public CategoriaRepository(AppDbcontext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Categoria> categorias => _context.Categorias;
+    }
+}
---
+++ LanchesMac/Repositories/LancheRepository.cs
@@ -0,0 +1,26 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Repositories
+{
+    public class LancheRepository : ILancheRepository
+    {
+        private readonly AppDbcontext _context;
+        public LancheRepository(AppDbcontext context)
+        {
+            _context = context;
+        }
+        IEnumerable<Lanche> ILancheRepository.Lanches => _context.Lanches.Include(c => c.Categoria);
+
+        IEnumerable<Lanche> ILancheRepository.LanchesPreferidos => _context.Lanches.
+                                              Where(l => l.IsLanchePreferido).
+                                              Include(c => c.Categoria);
+
+        Lanche ILancheRepository.GetLancheById(int lancheid)
+        {
+            return _context.Lanches.FirstOrDefault(l => l.LancheId == lancheid);
+        }
+    }
+}
