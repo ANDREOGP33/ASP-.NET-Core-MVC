39b9f0648bb1d97aa6808b56965434580a3b4e6c
ANDREOGP33 <andregasparoni690@gmail.com>
2023-12-30 22:14:34 -0300

    Criando o Controlador para gerenciar os itens do carrinho de compras

---
+++ LanchesMac/ViewsModels/CarrinhoCompraViewModel.cs
@@ -0,0 +1,10 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.ViewsModels
+{
+    public class CarrinhoCompraViewModel
+    {
+        public CarrinhoCompra CarrinhoCompra { get; set; }
+        public Decimal CarrinhoCompraTotal { get; set; }
+    }
+}
